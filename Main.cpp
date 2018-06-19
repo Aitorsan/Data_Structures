41092910e3c2503a039255672d0e8e899efdbd7b
AitorSf <aitor.sanmartin11@gmail.com>
2018-06-19 19:28:27 +0200

    try catch block added to merge sort

--- Main.cpp
+++ Main.cpp
@@ -4,12 +4,12 @@
 #include<vector>
 #include "include/Nodes.h"
 #include "include/SingleLinkedList.h"
-#include <Windows.h>
+
 
 
 int main(){
 
-	asf::vector<int> v{ 3,5,10,1,9,1 };
+	asf::vector<int> v{ 3,5,10,9,1,4,2,6,8,19,17,20,18,7,14,12,15,16,11,13 };
 
 	std::cout << v << std::endl;
 	v.sort_ascendent();
--- include/algorithms.h
+++ include/algorithms.h
@@ -22,54 +22,61 @@ namespace asf {
 		int l_it = beg;
 		int r_it = middle;
 
-		T* temp = new T[end - beg + 1];
-
-		int cont{};
+		T* temp = nullptr;
 		
-		while (l_it < middle || r_it <= end) {
+		try {	
+			
 
-			// if we pass a valid predicate to compare we use it, else we use the less < operator to sort
-			if (func != nullptr) {
-
-				if (r_it > end || l_it < middle &&  func(list[l_it], list[r_it])) {
-
-					temp[cont] = list[l_it];
-					++l_it;
-				}
-				else if (l_it > middle-1 || r_it <= end && func(list[r_it], list[l_it])) {
-					temp[cont] = list[r_it];
-					++r_it;
-				}//It means they are equal or the indexes are the same we just take the one still in the valid range
-				else if ( l_it < middle){
-					temp[cont] = list[l_it];
-					++l_it;
-				}
-				else {
-					temp[cont] = list[r_it];
-					++r_it;
-				}
-			}
-			else {//default way to sort
-
-				if (r_it > end || l_it < middle &&  list[l_it]< list[r_it]) {
-
-					temp[cont] = list[l_it];
-					++l_it;
-
-				}
-				else if (l_it > middle -1|| r_it <= end && list[r_it]<= list[l_it]) {
-					temp[cont] = list[r_it];
-					++r_it;
-				}
-			}
-
-			++cont;
-
-		}
+			temp = new T[end - beg + 1];
 		
-		// we copy the temp order list in the positions between beginning and end, we are currently considering,  of the original list
-		for (int i = beg, j = 0; i <= end; ++j , ++i) {
-			list[i] = temp[j];
+			int cont{};
+	
+			while (l_it < middle || r_it <= end) {
+
+				// if we pass a valid predicate to compare we use it, else we use the less < operator to sort
+				if (func != nullptr) {
+
+					if (r_it > end || l_it < middle &&  func(list[l_it], list[r_it])) {
+						temp[cont] = list[l_it];
+						++l_it;
+					}
+					else if (l_it > middle - 1 || r_it <= end && func(list[r_it], list[l_it])) {
+						temp[cont] = list[r_it];
+						++r_it;
+					}//It means they are equal or the indexes are the same we just take the one still in the valid range
+					else if (l_it < middle) {
+						temp[cont] = list[l_it];
+						++l_it;
+					}
+					else {
+						temp[cont] = list[r_it];
+						++r_it;
+					}
+				}
+				else {//default way to sort
+
+					if (r_it > end || l_it < middle &&  list[l_it] < list[r_it]) {
+
+						temp[cont] = list[l_it];
+						++l_it;
+
+					}
+					else if (l_it > middle - 1 || r_it <= end && list[r_it] <= list[l_it]) {
+						temp[cont] = list[r_it];
+						++r_it;
+					}
+				}
+				++cont;
+			}
+		    // we copy the temp order list in the positions between beginning and end, we are currently considering,  of the original list
+			for (int i = beg, j = 0; i <= end; ++j , ++i) {
+				list[i] = temp[j];
+			}
+		
+		}catch (const std::bad_alloc& e) {
+			std::cout << e.what()<< " Fail to allocate, merge sort\n";
+			delete[] temp;
+			temp = nullptr;
 		}
 		delete[] temp;
 	
