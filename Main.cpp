825f62ffd0cd43d227044221783afa547094ea38
Aitor sanmartin Ferreira <aitor.sanmartin11@gmail.com>
2018-09-13 21:39:16 +0200

    patch example

--- Main.cpp
+++ Main.cpp
@@ -6,13 +6,63 @@
 #include "include/SingleLinkedList.h"
 
 
+#include<iostream>
+
+
+
+class parent{
+    
+public:
+    
+    virtual void method()= 0;
+    virtual void mehthod_two() = 0;
+    
+    
+};
+
+
+
+class child:public parent{
+    
+    
+    public :
+    
+    void method(){
+        std::cout << "child one"<<std::endl;
+    }
+    
+    void method_two(){
+        std::cout << "mehtod two child one" << std::endl;
+        
+    }
+    
+    
+};
+
+
+class subchild:public parent{
+    virtual void method(){
+    std::cout << "is private you cant use it" << std::endl;}
+    virtual void mehthod_two(){
+    std::cout << "method two is private you cant use it" <<std::endl;
+    }
+    public :
+    
+    void nowyes(){
+       mehthod_two();
+        method();
+    }
+    
+    
+};
 
 int main(){
 
+    
+    subchild c;
+    c.nowyes();
 
-
-
-	std::cin.get();
+ 
 	return 0;
 }
 
